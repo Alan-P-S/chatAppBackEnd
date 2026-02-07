2b3db8371e879c77c81cb6e86f4763e719607497
alanps <alanps3699@gmail.com>
2026-02-07 22:20:06 +0530

    JWT token sameSite set to be true

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,7 +6,7 @@ export const generateToken = (userId,res)=>{
     res.cookie('token',token,{
         maxAge:7 * 24 *60*60*1000,
         httpOnly:true, // prevent XXS attacks cross-site scripting attacks
-        sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+        sameSite: "None", // CSRF attacks cross-site request forgery attacks
         secure: process.env.NODE_ENV !=="development"
 
     });
